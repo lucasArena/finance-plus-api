9fe9372435cfea7206f5051a23b9429a9a92fdcf
Lucas Santos <lucas.santos-sc@airliquide.com@MacBook-Pro-de-Lucas-2.local>
2025-02-24 21:44:49 -0300

    fix: adjust signup user code

--- src/domain/usecases/SignUpUser/SignUpUserUsecase.ts
+++ src/domain/usecases/SignUpUser/SignUpUserUsecase.ts
@@ -4,7 +4,10 @@ import { ISignUpUserUsecaseDTO } from '@/domain/usecases/SignUpUser/SignUpUserUs
 import { User } from '@/domain/entities/User.types'
 import { IUserRepository } from '@/domain/ports/UserRepository.types'
 import { IEmail } from '@/domain/application/Email.types'
-import { IUserCodesRepository } from '@/domain/ports/UserCodesRepository.types'
+import {
+  EUserCodeType,
+  IUserCodesRepository,
+} from '@/domain/ports/UserCodesRepository.types'
 import { UserCode } from '@/domain/entities/UserCode.types'
 
 @injectable()
@@ -38,6 +41,7 @@ export class SignUpUserUsecase {
     const userActivationCode = new UserCode({
       userKey,
       code: Math.floor(10000 + Math.random() * 90000),
+      type: EUserCodeType.EMAIL_VALIDATION,
     })
     await this.userCodesRepository.create(userActivationCode)
 
